4befd5a78f2a4596ea811a57000490c2d6eae3a3
triloon <dimontopcik59@gmail.com>
2026-05-07 15:10:23 +0300

    update

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 
 let flipdown;
 let currentTargetTimestamp;
-
+let targetDate = new Date(2026, 4, 17);
 function initTimer(targetDate) {
   const now = new Date().getTime();
-  let target = targetDate.getTime();
-  
-  if (target <= now) {
-    target = now + 3600000;
-    targetDate = new Date(target);
-  }
-  
+  let target = targetDate.getTime();  
   currentTargetTimestamp = target;
   
   if (flipdown) {
@@ -24,30 +18,14 @@ function initTimer(targetDate) {
     showDays: true
   });
   
-  flipdown.start();
-  
-}
-
-function resetToDefault() {
-  const now = new Date();
-  const target = new Date(now.getTime() + (7 * 24 * 60 * 60 * 1000) + (5 * 60 * 60 * 1000) + (37 * 60 * 1000));
-  initTimer(target);
-}
-
-function addFiveMinutes() {
-  if (currentTargetTimestamp) {
-    const newTarget = new Date(currentTargetTimestamp + (5 * 60 * 1000));
-    initTimer(newTarget);
-  } else {
-    resetToDefault();
-  }
+  flipdown.start(); 
 }
 function startTimerFromSequence() {
   const now = new Date();
-  const days = 7;
-  const hours = 0;
-  const minutes = 9;
-  const seconds = 3;
+  const days = targetDate.getDate() - now.getDate();
+  const hours = targetDate.getHours() - now.getHours();
+  const minutes = targetDate.getMinutes() - now.getMinutes();
+  const seconds = targetDate.getSeconds() - now.getSeconds();
   const ms = (days * 24 * 60 * 60 * 1000) + (hours * 60 * 60 * 1000) + (minutes * 60 * 1000) + (seconds * 1000);
   const target = new Date(now.getTime() + ms);
   initTimer(target);
